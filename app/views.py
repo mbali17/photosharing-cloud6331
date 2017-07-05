6f779e1aeffd9dfc7c8c7aa58e978d4e7ecb0f7c
mbali17 <alimirbasheer@gmail.com>
2017-07-05 17:01:56 -0500

    updating connection params

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app 
 from flask import render_template
 from databasehelper import verifyConnection
 #configure paths
@@ -6,7 +6,7 @@ from databasehelper import verifyConnection
 def welcome():
     return render_template('index.html',textToDisplay='Welcome to the app on azure')
 
-@app.route('/getResults',methods=["GET","POST"])
+@app.route('/getResults')
 def getResults():
     results = verifyConnection()
     return render_template('DisplayResults.html',results=results)
--- databasehelper.py
+++ databasehelper.py
@@ -1,33 +1,27 @@
 # Module consisting of the all the database utilities
 import os
 import json
-import pymysql as sql
+#import pymysql as sql
+import mysql.connector as con
 from app  import app
-def connectToDatabase():
-    clearDBService = json.loads(os.environ['VCAP_SERVICES'])['cleardb'][0]
-    clearDBServiceCredentials = clearDBService['credentials']
-    host = clearDBServiceCredentials['hostname']
-    port = int(clearDBServiceCredentials['port'])
-    userName = clearDBServiceCredentials['username']
-    password = clearDBServiceCredentials['password']
-    database = clearDBServiceCredentials['name']
-    connection = sql.connect(host=host,
-                             port=port,
-                             user=userName,
-                             password=password,
-                             db=database,
-                             charset='utf8mb4',
-                             cursorclass=sql.cursors.DictCursor)
-    return connection
+#TODO:Move the config to a file
 def connectToLocalDatabase():
+    connection = con.connect(user="mba@cloud6331-az",
+                                         password="Passis1234$#Passis1234$#",
+                                         host="cloud6331-az.mysql.database.azure.com",
+                                         port=3306,
+                                         database="testdb",ssl_verify_cert=False,ssl_ca="")
+    '''
      connection = sql.connect(host="cloud6331-az.mysql.database.azure.com",
                              port=3306,
                              user="mba@cloud6331-az",
                              password="Passis1234$#",
                              db="testdb",
-                             charset='utf8mb4',
-                             cursorclass=sql.cursors.DictCursor)
-     return connection
+                             charset='utf8mb4')
+                             #cursorclass=sql.cursors.DictCursor,max_allowed_packet =45*1024*1024)
+                             # ssl={'key': 'C:/Users/tabrez/Desktop/MyServerCACert.pem','ca': 'C:/Users/tabrez/Downloads/BaltimoreCyberTrustRoot.crt'})
+    '''
+    return connection
 def uploadFileToDatabase():
     print("uploading file to the database")
 
@@ -35,12 +29,12 @@ def verifyConnection():
     connection = connectToLocalDatabase()
     results = []
     try:
-        with connection.cursor() as cursor:
-            sql_statement='select * from classes'
-            cursor.execute(sql_statement)
-            results = cursor.fetchall()
-            for row in results:
-                results.append(row)
-        return results
+        cur=connection.cursor(dictionary=True)
+        sql_statement='''select * from classes'''
+        cur.execute(sql_statement)
+        resultset = cur.fetchall()
+        for row in resultset:
+            results.append(row)
     finally:
         connection.close()
+    return results
